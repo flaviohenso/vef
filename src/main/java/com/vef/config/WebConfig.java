83e15b8da199f215121f278b5dca42c205d543dc
flavio <flavio.henso@gmail.com>
2020-04-23 11:05:45 -0300

    Configurando ViewResolver com Thymeleaf

--- src/main/java/com/vef/config/WebConfig.java
+++ src/main/java/com/vef/config/WebConfig.java
@@ -1,15 +1,62 @@
 package com.vef.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import com.vef.controller.CervejasController;
 
+import javassist.tools.web.Viewer;
+
 @Configuration
-@ComponentScan(basePackageClasses = {CervejasController.class})
-@EnableWebMvc	
-public class WebConfig extends WebMvcConfigurerAdapter {
+@ComponentScan(basePackageClasses = { CervejasController.class })
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContext {
+
+	private ApplicationContext applicationContext;
+
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(this.templateEngine());
+		resolver.setCharacterEncoding("UTF-8");
+		return resolver;
+	}
+
+	@Bean
+	public ISpringTemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(this.templateResolver());
+		return engine;
+	}
+
+	@Bean
+	public ITemplateResolver templateResolver() {	
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver.setApplicationContext(applicationContext);
+		resolver.setPrefix("classpath:/templates/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode(TemplateMode.HTML);
+		return resolver;
+	}
+
+	
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
 
 }
--- src/main/java/com/vef/controller/CervejasController.java
+++ src/main/java/com/vef/controller/CervejasController.java
@@ -1,5 +1,14 @@
 package com.vef.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class CervejasController {
 
+	@RequestMapping("/cerveja/novo")
+	public String novo() {
+		return "cerveja/cadastro";
+	}
+	
 }
